fa3bb1849bc0f473db3da4498e5c89f8c6da7b3b
Vidal Soncco <vidal.soncco@arequipa.ccpna>
2024-01-25 17:12:38 -0500

    document logs repoort dependencies

--- extra-addons/document_log_report/reports/document_log_report.py
+++ extra-addons/document_log_report/reports/document_log_report.py
@@ -1,13 +1,16 @@
 
 from odoo import api, fields, models, tools
 from odoo.tools.safe_eval import safe_eval
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class DocumentLogReport(models.Model):
     _name = "document.log.report"
     _description = "Document Log Report"
     _auto = False
 
+    date = fields.Datetime('Date', readonly=True)
     document_id = fields.Many2one(comodel_name="documents.document", readonly=True)
     author_id = fields.Many2one(comodel_name="res.partner", readonly=True)
     message_type = fields.Selection([
@@ -16,7 +19,6 @@ class DocumentLogReport(models.Model):
         ('notification', 'System notification'),
         ('user_notification', 'User Specific Notification')],
         'Type', readonly=True)
-    date = fields.Datetime('Date', readonly=True)
     body = fields.Html('Contents', sanitize_style=True)
 
     # TODO: need a field to help these cases more clearly
@@ -36,17 +38,22 @@ class DocumentLogReport(models.Model):
     def init_results(self, filter_fields):
         date_from = filter_fields.date_from or "1900-01-01"
         date_to = filter_fields.date_to or fields.Date.context_today(self)
+        users = tuple(filter_fields.user_ids.mapped('partner_id').ids)
+        documents = tuple(filter_fields.document_ids.ids)
 
         query_ = """
-            select mm.id, mm.message_type, mm.date, mm.body, dd.id as document_id, rp.id as author_id 
+            select mm.id, mm.date, dd.id as document_id, rp.id as author_id, mm.message_type, mm.body 
             from mail_message mm 
             left join documents_document dd on mm.res_id = dd.id 
             left join res_partner rp on mm.author_id = rp.id
-            where mm.model = 'documents.document' and mm.date >= %s and mm.date <= %s
+            where mm.model = 'documents.document' and mm.date::date >= %s and mm.date::date <= %s and mm.author_id in %s and mm.res_id in %s
         """
-        params = (date_from,
-                    date_to)
+        params = (date_from, date_to, users, documents)
+
         tools.drop_view_if_exists(self._cr, self._table)
+        _logger.info("----------------->")
+        _logger.info(users)
+        _logger.info(documents)
         res = self._cr.execute(
             """CREATE VIEW {} as ({})""".format(self._table, query_), params)
         return res
--- extra-addons/document_log_report/wizard/document_log_report_wizard.py
+++ extra-addons/document_log_report/wizard/document_log_report_wizard.py
@@ -10,29 +10,18 @@ class DocumentLogReportWizard(models.TransientModel):
 
     date_from = fields.Date(string="Start Date")
     date_to = fields.Date(string="End Date")
-    location_id = fields.Many2one(
-        comodel_name="stock.location", string="Location")
-    product_ids = fields.Many2many(
-        comodel_name="product.product", string="Products")
-    len_product = fields.Integer()
-    product_category_ids = fields.Many2many(
-        comodel_name="product.category", string="Product Categories")
-    is_groupby_location = fields.Boolean(string="Group Locations", default=True,
-                                         help="If checked qty will groupby location mean count internal transfer qty else will not count internal transfer qty")
-
-    @api.onchange('product_ids')
-    def _onchange_product_ids(self):
-        for record in self:
-            record.len_product = len(record.product_ids)
-
+    user_ids = fields.Many2many(
+        comodel_name="res.users", string="User")
+    document_ids = fields.Many2many(
+        comodel_name="documents.document", string="Document")
+    
     def _prepare_document_log_report(self):
+        # users = self.user_ids if self.user_ids else 
         return {
             "date_from": self.date_from or "1900-01-01",
             "date_to": self.date_to or fields.Date.context_today(self),
-            "product_ids": [(6, 0, self.product_ids.ids)] or None,
-            "location_id": self.location_id.id or None,
-            "product_category_ids": [(6, 0, self.product_category_ids.ids)] or None,
-            "is_groupby_location": self.is_groupby_location,
+            "user_ids": [(6, 0, self.user_ids.ids)] if self.user_ids else [(6, 0, self.env['res.users'].search([]).ids)],
+            "document_ids": [(6, 0, self.document_ids.ids)] if self.document_ids else [(6, 0, self.env['documents.document'].search([]).ids)],
         }
 
     def button_view(self):
@@ -50,26 +39,26 @@ class DocumentLogReportWizard(models.TransientModel):
         vals["context"] = context
         return vals
 
-    def button_view_details(self):
-        vals = {}
-        report = self.create(self._prepare_document_log_report())
-        init = self.env["document.log.details.report"].init_results(report)
-        details = self.env["document.log.details.report"].search([])
-        action = self.env.ref(
-            'document_log_report.action_document_log_details_report')
-        vals = action.sudo().read()[0]
-        context = vals.get("context", {})
-        if context:
-            context = safe_eval(context)
-        context["active_ids"] = details.ids
-        data = {
-            'product_default_code': self.product_ids.default_code,
-            'product_name': self.product_ids.name,
-            'date_from': self.date_from or None,
-            'date_to': self.date_to or fields.Date.context_today(self),
-            'location': self.location_id.complete_name or None,
-            'category': self.product_ids.categ_id.complete_name or None,
-        }
-        context["data"] = data
-        vals["context"] = context
-        return vals
+    # def button_view_details(self):
+    #     vals = {}
+    #     report = self.create(self._prepare_document_log_report())
+    #     init = self.env["document.log.details.report"].init_results(report)
+    #     details = self.env["document.log.details.report"].search([])
+    #     action = self.env.ref(
+    #         'document_log_report.action_document_log_details_report')
+    #     vals = action.sudo().read()[0]
+    #     context = vals.get("context", {})
+    #     if context:
+    #         context = safe_eval(context)
+    #     context["active_ids"] = details.ids
+    #     data = {
+    #         'product_default_code': self.product_ids.default_code,
+    #         'product_name': self.product_ids.name,
+    #         'date_from': self.date_from or None,
+    #         'date_to': self.date_to or fields.Date.context_today(self),
+    #         'location': self.location_id.complete_name or None,
+    #         'category': self.product_ids.categ_id.complete_name or None,
+    #     }
+    #     context["data"] = data
+    #     vals["context"] = context
+    #     return vals
